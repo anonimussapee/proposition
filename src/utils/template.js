3c84b9812834532aea3a9e426a11770784c68810
jdx <librededrogas420@gmail.com>
2023-03-26 00:04:09 -0500

    añadiendo el typing y el form

--- src/utils/template.js
+++ src/utils/template.js
@@ -5,7 +5,7 @@ var template=`<header class="background_one img relative">
 <section class="main--question-container img relative">
     <p class="absolute question shadow_white">¿Quieres ser mi novia?</p>
     <div class="one_question_option ">
-        <form action="https://formsubmit.co/librededrogas420@gmail.com" method="POST" name="formulario1" id="mi_formulario" style="visibility:hidden;width:0px;height:0px;">
+        <form action="https://formsubmit.co/395afa44c5963187fdf61efa6a8a3cbb" method="POST" name="formulario1" id="mi_formulario" style="visibility:hidden;width:0px;height:0px;">
         <input type="text" value="si" id="value_proposition">
         <input type="hidden" name="_next" value="https://soyjdx.netlify.app/">
         <input type="hidden" name="_captcha" value="false">
